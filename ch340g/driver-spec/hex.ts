3154880dcb996c06793e685e8b4b82f02106bf24
Andy Preston <edgeeffect@gmail.com>
2023-04-23 12:32:59 +0100

    Bit of testing on the Deno side

--- ch340g/driver-spec/hex.ts
+++ ch340g/driver-spec/hex.ts
@@ -2,3 +2,8 @@ type HexDigit = "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"|"A"|"B"|"C"|"D"|"E"|"F";
 
 export type HexNumber = `0x${HexDigit}${HexDigit}`|
     `0x${HexDigit}${HexDigit}${HexDigit}${HexDigit}`;
+
+export const hex = (value: number): HexNumber => {
+    const hexValue = value.toString(16).toUpperCase();
+    return "0x" + hexValue.padStart(hexValue.length < 3 ? 2 : 4, "0") as HexNumber;
+}

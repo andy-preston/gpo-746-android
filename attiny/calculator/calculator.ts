a95293c9bf276d2d476c06068d511ad0c3c60dab
Andy Preston <edgeeffect@gmail.com>
2023-08-27 17:04:40 +0100

    I'm sad to see TypeScript go but fewer languages is better

--- attiny/calculator/calculator.ts
+++
@@ -1,7 +0,0 @@
-import {default as baud} from "./baud.ts";
-import {default as timer1} from "./timer1.ts";
-
-const cpuClockFrequency = 14745600;
-
-console.log(baud(cpuClockFrequency, 9600));
-console.log(timer1(cpuClockFrequency).join("\n"));
--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 rootProject.name = "gpo-746"
 include(":android")
 include(":shared")
+include(":avr")

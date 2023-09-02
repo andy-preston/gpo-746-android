c79f1d655200225b134e0a1dfc64e83eafaf3f57
Andy Preston <edgeeffect@gmail.com>
2023-09-02 18:49:41 +0100

    first test for buildSrc

--- src/buildSrc/build.gradle.kts
+++ src/buildSrc/build.gradle.kts
@@ -2,6 +2,19 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
 }
 
+sourceSets {
+    val main by getting {
+        dependencies {
+            //put your multiplatform dependencies here
+        }
+    }
+    val test by getting {
+        dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
 repositories {
     mavenCentral()
 }

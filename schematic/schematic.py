49a2e5aaad314dfb2f598701e69b56a20419277a
Andy Preston <edgeeffect@gmail.com>
2023-10-09 20:56:26 +0100

    (I suppose, it's kind-of) Refactring schematic

--- schematic/schematic.py
+++ schematic/schematic.py
@@ -2,16 +2,14 @@
 
 import schemdraw
 import schemdraw.elements as elm
-from analogue import analogue
+from analog import analog
 from digital import digital
 
 
 def schematic(dwg: schemdraw.Drawing):
-    """general setup of schematic actual diagrams in digital.py and analogue.py"""
+    """general setup of schematic actual diagrams in digital.py and analog.py"""
     dwg.config(fontsize=12)
     elm.style(elm.STYLE_IEC)
-
-    anchor_point: elm.Element = analogue(dwg)
+    anchor_point: elm.Element = analog(dwg)
     dwg.move_from(anchor_point, 4, -25)
-
     digital(dwg)

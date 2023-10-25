c5a3413fe2001155cb5f6f3d80878d849e48ec8c
Andy Preston <edgeeffect@gmail.com>
2023-10-25 08:55:08 +0100

    Finished the schematic

--- schematic/analog.py
+++ schematic/analog.py
@@ -13,6 +13,7 @@ class AnalogPart:
         self._0v = schemdraw.util.Point([0, 0])
         self._5v = schemdraw.util.Point([0, 0])
         self._20v = schemdraw.util.Point([0, 0])
+        self.hook_pin = schemdraw.util.Point([0, 0])
 
     def psu(self):
         """The 5V power supply"""
@@ -26,7 +27,7 @@ class AnalogPart:
             leadlen=1,
             label="7805",
         )
-        self.dwg += _7805
+        self.dwg += _7805.right()
         self.dwg += (
             ElectrolyticCapacitor()
             .at(_7805.input)
@@ -55,7 +56,8 @@ class AnalogPart:
 
         header = elm.Header(rows=1, pinsleft=[pin], pinalignleft="center")
         solenoid = elm.Transformer(t1=7, t2=0).right()
-
+        if pin == "B2":
+            solenoid.label("Telephone\nBell\nSolenoids", loc="bottom")
         transistor = elm.BjtNpn(circle=True).right().label("BC548B", loc="top")
         diode = elm.Diode().label("1N4001").toy(self._20v)
         self.dwg += header
@@ -66,7 +68,7 @@ class AnalogPart:
             self.dwg += elm.Vss().label("0V").at(transistor.emitter)
         else:
             self.dwg += elm.Wire("|-").at(transistor.emitter).to(self._0v)
-        self.dwg += elm.Line().at(transistor.collector).right()
+        self.dwg += elm.Line().at(transistor.collector).right(2)
         if pin == "B1":
             self.dwg += diode.down()
             self.dwg.move(1, 0.2)
@@ -89,7 +91,7 @@ class AnalogPart:
     ):
         """Half of the dial debounce circuit"""
 
-        self.dwg += elm.Line().at(input_pin).right(1)
+        self.dwg += elm.Line().at(input_pin).right(1.5)
         self.dwg.push()
         if location == "top":
             self.dwg += elm.Line().up(1.5)
@@ -104,10 +106,11 @@ class AnalogPart:
         self.dwg += elm.Resistor().right().label("18K", loc=location)
         self.dwg.push()
         if location == "top":
-            self.dwg += elm.Line().up(1.5)
+            self.dwg += elm.Line().up(1.2)
+            self.dwg += elm.Diode().left().label("1N4148", loc="top")
         else:
-            self.dwg += elm.Line().down(1.5)
-        self.dwg += elm.Diode().left().reverse().label("1N4148", loc=location)
+            self.dwg += elm.Line().down(1.2)
+            self.dwg += elm.Diode().left().reverse().label("1N4148", loc="bottom")
         self.dwg.pop()
         self.dwg += elm.Line().right(1.1 if location == "top" else 2.2)
         self.dwg.push()
@@ -120,25 +123,78 @@ class AnalogPart:
         self.dwg.pop()
         self.dwg += elm.Line().to(switch_pin)
 
-    def draw(self) -> schemdraw.util.Point:
-        """Draw the 'Analog' part - return anchor for positioning digital"""
-
-        self.dwg.move(26, 0)
-        io_header = elm.Header(rows=2, pinsleft=["D4", "D5"], pinalignleft="center")
+    def dial_circuit(self):
+        """The whole dial debounce circuit"""
+        io_header = elm.Header(
+            rows=3,
+            pinsleft=["", "D4", "D5"],
+            pinalignleft="center",
+            pinsright=["D3", "", ""],
+            pinalignright="center",
+        )
         self.dwg += io_header
-        self.dwg.move(7.5, -0.6)
+        self.hook_pin = io_header.pin1
+        self.dwg.move(8, -0.6)
         dial_header = elm.Header(
-            rows=4, pinsright=["blue", "grey", "pink", "orange"], pinalignright="bottom"
+            rows=4, pinsright=["blue", "grey", "pink", "orange"], pinalignright="center"
         )
         self.dwg += dial_header
-        self.debounce("top", io_header.pin1, dial_header.pin1, dial_header.pin2)
-        self.debounce("bottom", io_header.pin2, dial_header.pin4, dial_header.pin3)
+        self.debounce("top", io_header.pin2, dial_header.pin1, dial_header.pin2)
+        self.debounce("bottom", io_header.pin3, dial_header.pin4, dial_header.pin3)
+
+    def dial(self):
+        """The actual dial in the phone"""
+        dial_header = elm.Header(
+            rows=5,
+            pinsleft=["blue", "grey", "pink", "orange", "brown"],
+            pinalignleft="center",
+        )
+        dpst = elm.SwitchDpst()
+
+        self.dwg += dial_header
+        self.dwg.move(2, 2.5)
+        self.dwg += dpst
+        blue = dial_header.pin1
+        grey = dial_header.pin2
+        pink = dial_header.pin3
+        orange = dial_header.pin4
+        brown = dial_header.pin5
+        self.dwg += elm.Line().at(grey).right(1.2)
+        self.dwg += elm.Wire("|-").to(dpst.p1)
+        self.dwg += elm.Line().at(blue).up(1)
+        self.dwg += elm.Line().right(3.6).label("Telephone\nDial\n\n")
+        self.dwg += elm.Wire("|-").to(dpst.t1)
+        self.dwg += elm.Line().at(pink).right(1.2)
+        self.dwg.push()
+        self.dwg += elm.Line().down(1)
+        self.dwg += elm.Switch().right(2)
+        self.dwg += elm.Line().down(0.75)
+        self.dwg += elm.Line().left(2.5)
+        self.dwg += elm.Wire("|-").to(orange)
+        self.dwg.pop()
+        self.dwg += elm.Line().to(dpst.p2)
+        self.dwg += elm.Line().at(brown).down(1)
+        self.dwg += elm.Line().right(3.6)
+        self.dwg += elm.Wire("|-").to(dpst.t2)
+
+    def hook(self):
+        """connections to the cradle/hook switch"""
+        self.dwg += elm.Line().at(self.hook_pin).left(2)
+        self.dwg += elm.Resistor().toy(self._5v).label("10K").hold()
+        self.dwg += elm.Line().down(1)
+        self.dwg += elm.Switch().toy(self._0v).label("Telephone\nHook", loc="bottom")
+
+    def draw(self) -> schemdraw.util.Point:
+        """Draw the 'Analog' part - return anchor for positioning digital"""
+        self.dial()
         self.dwg.move(-14, -0.5)
+        self.dial_circuit()
+        self.hook()
+        self.dwg.move(-5, 2.5)
         self.psu()
-        self.dwg.move(-23, 2)
+        self.dwg.move(-22, 2)
         self.solenoid_transistor("B1")
-        self.dwg.move(-8.05, -4.3)
+        self.dwg.move(-7.05, -4.3)
         end_position = self.dwg.here
         self.solenoid_transistor("B2")
-
         return end_position

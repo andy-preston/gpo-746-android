baae57e2c7c8e3cbaba21fc528b6004a87588216
Andy Preston <edgeeffect@gmail.com>
2023-11-07 18:11:53 +0000

    fixed tone buffer tests

---
+++ src/shared/src/androidUnitTest/kotlin/gpo_746/ToneBufferBuilderTest.kt
@@ -0,0 +1,24 @@
+package gpo_746
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+import kotlin.test.assertSame
+
+class ToneBufferBuilderTest(): ToneBufferBuilder() {
+
+    override protected val waveform = byteArrayOf(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
+
+    @Test
+    public fun buffer_will_exceed_minimum_requirement() {
+        assertTrue(setupSamples(128).size >= 128)
+    }
+
+    @Test
+    public fun buffer_contains_repeated_input_data() {
+        var pos = 0
+        setupSamples(64).forEach {
+            assertSame(waveform[pos], it)
+            pos = if (pos < waveform.size - 1) pos + 1 else 0
+        }
+    }
+}

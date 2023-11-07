9686aabd1696c4ff4f443333330da20f0fbae84e
Andy Preston <edgeeffect@gmail.com>
2023-11-07 16:02:15 +0000

    improved schematic formatting

--- schematic/draw.py
+++ schematic/draw.py
@@ -1,28 +1,21 @@
 """Command line to draw the schematic and nicely format the SVG"""
 
+from subprocess import Popen, PIPE
 import schemdraw
-import xmlformatter
 from schematic import schematic
-
-with open("schematic.html", "r", encoding="UTF-8") as file:
-    old_contents: str = file.read().replace("<!DOCTYPE html>", "")
 
 with schemdraw.Drawing() as drawing:
     schematic(drawing)
     drawing_xml: str = drawing.get_imagedata().decode("UTF-8")
 
-rawXml: str = (
-    old_contents.split("<svg")[0] + drawing_xml + old_contents.split("</svg>")[-1]
-)
+with open("schematic.html", "r", encoding="UTF-8") as file:
+    old_contents: str = file.read()
+header = old_contents.split("<svg")[0]
+footer = old_contents.split("</svg>")[-1]
 
-formatted: str = (
-    xmlformatter.Formatter(
-        indent="4", indent_char=" ", encoding_output="UTF-8", preserve=["literal"]
-    )
-    .format_string(rawXml)
-    .decode("UTF-8")
-)
+with Popen(["./prettier.ts"], stdin=PIPE, stdout=PIPE) as prettier:
+    prettier.stdin.write((header + drawing_xml + footer).encode())
+    pretty: str = prettier.communicate()
 
 with open("schematic.html", "w", encoding="UTF-8") as file:
-    file.write("<!DOCTYPE html>\n")
-    file.write(formatted)
+    file.write(pretty[0].decode("UTF-8").replace("!doctype", "!DOCTYPE"))

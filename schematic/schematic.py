41288d6c797e686c000770aaef79b096fb473104
Andy Preston <edgeeffect@gmail.com>
2023-11-09 12:26:48 +0000

    Some static analysis and optimisation on schematic

--- schematic/schematic.py
+++ schematic/schematic.py
@@ -1,15 +1,16 @@
 """"GPO-746 Microcontroller Schematic"""
 
-import schemdraw
+from schemdraw import Drawing
+from schemdraw.util import Point
 import schemdraw.elements as elm
 from analog import AnalogPart
 from digital import digital
 
 
-def schematic(dwg: schemdraw.Drawing):
+def schematic(dwg: Drawing):
     """general setup of schematic actual diagrams in digital.py and analog.py"""
     dwg.config(fontsize=12)
     elm.style(elm.STYLE_IEC)
-    anchor_point: schemdraw.util.Point = AnalogPart(dwg).draw()
+    anchor_point: Point = AnalogPart(dwg).draw()
     dwg.move_from(anchor_point, 4.8, -22)
     digital(dwg)

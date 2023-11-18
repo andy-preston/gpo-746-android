5838432c17def1e060c99dc69d6128dade2e0384
Andy Preston <edgeeffect@gmail.com>
2023-11-18 17:20:11 +0000

    The static analysis man came round to complain about the smell of my code

--- src/buildSrc/src/test/kotlin/gpo746/Ch340gConstantsTest.kt
+++ src/buildSrc/src/test/kotlin/gpo746/Ch340gConstantsTest.kt
@@ -1,7 +1,6 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.test.assertNotEquals
 
 class Ch340gConstantsTest {
 
@@ -32,7 +31,7 @@ class Ch340gConstantsTest {
     fun invalid_baud_rate_throws_an_exception() {
         val exception = assertFailsWith<Exception>(
             block = {
-                Ch340gConstants().baudRate(1024);
+                Ch340gConstants().baudRate(1024)
             }
         )
         assertEquals(
@@ -44,25 +43,25 @@ class Ch340gConstantsTest {
     @Test
     fun calculated_baud_values_from_free_bsd_match_lookup_table_from_other_drivers() {
         val expectation = mapOf(
-            2400 to mapOf("divisorPrescale" to 0xd901, "mod" to 0x0038 ),
-            4800 to mapOf("divisorPrescale" to 0x6402, "mod" to 0x001f ),
-            9600 to mapOf("divisorPrescale" to 0xb202, "mod" to 0x0013 ),
-            19200 to mapOf("divisorPrescale" to 0xd902, "mod" to 0x000d ),
-            38400 to mapOf("divisorPrescale" to 0x6403, "mod" to 0x000a ),
-            115200 to mapOf("divisorPrescale" to 0xcc03, "mod" to 0x0008 ),
+            2400 to mapOf("divisorPreScale" to 0xd901, "mod" to 0x0038),
+            4800 to mapOf("divisorPreScale" to 0x6402, "mod" to 0x001f),
+            9600 to mapOf("divisorPreScale" to 0xb202, "mod" to 0x0013),
+            19200 to mapOf("divisorPreScale" to 0xd902, "mod" to 0x000d),
+            38400 to mapOf("divisorPreScale" to 0x6403, "mod" to 0x000a),
+            115200 to mapOf("divisorPreScale" to 0xcc03, "mod" to 0x0008),
         )
         for ((rate, expected) in expectation) {
-            val (divisorPrescale, mod) = Ch340gConstants().baudRate(rate)
+            val (divisorPreScale, mod) = Ch340gConstants().baudRate(rate)
             assertEquals(
-                expected["divisorPrescale"],
-                divisorPrescale,
-                "prescale and div matching for ${rate}"
-            );
+                expected["divisorPreScale"],
+                divisorPreScale,
+                "prescale and div matching for $rate"
+            )
             assertEquals(
                 expected["mod"],
                 mod,
-                "mod matching for ${rate}"
-            );
+                "mod matching for $rate"
+            )
         }
     }
 }

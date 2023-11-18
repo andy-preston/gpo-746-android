55011ff950a8e33022a23336f7a03da1deada4c4
Andy Preston <edgeeffect@gmail.com>
2023-11-18 17:15:09 +0000

    The static analysis man came round to complain about the smell of my code

--- src/shared/src/linuxMain/kotlin/gpo746/LinuxMain.kt
+++ src/shared/src/linuxMain/kotlin/gpo746/LinuxMain.kt
@@ -1,15 +1,18 @@
-import gpo_746.Ch340g
-import gpo_746.TestCases
-import gpo_746.UsbSystemMock
+import andyp.gpo746.Ch340g
+import andyp.gpo746.TestCases
+import andyp.gpo746.UsbSystemMock
 import platform.posix.sleep
+
+private const val DEFAULT_PAUSE = 10u
+private const val DEFAULT_COUNT = 1
 
 fun main(args: Array<String>) {
     val ch340g = Ch340g(UsbSystemMock())
     val testCases = TestCases(ch340g)
     val testMap = testCases.list()
-    val requiredTest: String = if (args.size >= 1) args[0] else ""
-    val repeats: Int = if (args.size >= 2) args[1].toInt() else 1
-    val pause: UInt = if (args.size >= 3) args[2].toUInt() else 10u
+    val requiredTest: String = if (args.size > 0) args[0] else ""
+    val repeats: Int = if (args.size > 1) args[1].toInt() else DEFAULT_COUNT
+    val pause: UInt = if (args.size > 2) args[2].toUInt() else DEFAULT_PAUSE
 
     testMap.forEach { (testName, testMethod) ->
         if (requiredTest == testName) {
@@ -24,11 +27,11 @@ fun main(args: Array<String>) {
             return
         }
     }
-    println ("./bin/usb-test test-case [number of repeats] [pause between tests]")
+    println("./bin/usb-test test-case [number of repeats] [pause between tests]")
     println("Available test cases:")
     testMap.keys.forEach { testName ->
         println(testName)
     }
-    println ("default number of repeats is 1")
-    println ("default pause is 10")
+    println("default number of repeats is 1")
+    println("default pause is 10")
 }

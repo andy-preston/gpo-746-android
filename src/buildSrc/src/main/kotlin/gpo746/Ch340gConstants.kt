b675bc3aed0d98c535044e01ee80b11a3be4a2d4
Andy Preston <edgeeffect@gmail.com>
2023-11-18 17:20:50 +0000

    The static analysis man came round to complain about the smell of my code

--- src/buildSrc/src/main/kotlin/gpo746/Ch340gConstants.kt
+++ src/buildSrc/src/main/kotlin/gpo746/Ch340gConstants.kt
@@ -1,23 +1,26 @@
+@Suppress("MagicNumber")
 enum class Lcr1Bit(val mask: Int) {
     CS5(0x00), // Not defined in FreeBSD, only in NetBSD
     CS6(0x01), // Not defined in FreeBSD, only in NetBSD
     CS7(0x02), // Not defined in FreeBSD, only in NetBSD
     CS8(0x03), // The only one in FreeBSD
-    parityEnable(0x08),
-    enableTX(0x40),
-    enableRX(0x80)
+    ParityEnable(0x08),
+    EnableTX(0x40),
+    EnableRX(0x80)
 }
 
+@Suppress("MagicNumber")
 enum class Lcr2Bit(val mask: Int) {
-    parityNone(0x00),
-    parityEven(0x07),  // FreeBSD says 0x07 Linux & NetBSD says 0x10
-    parityOdd(0x06),   // FreeBSD says 0x06         NetBSD says 0x00
-    parityMark(0x05),  // FreeBSD says 0x05         NetBSD says 0x20
-    paritySpace(0x04)  // FreeBSD says 0x04         NetBSD says 0x30
+    ParityNone(0x00),
+    ParityEven(0x07), // FreeBSD says 0x07 Linux & NetBSD says 0x10
+    ParityOdd(0x06), //  FreeBSD says 0x06         NetBSD says 0x00
+    ParityMark(0x05), // FreeBSD says 0x05         NetBSD says 0x20
+    ParitySpace(0x04) // FreeBSD says 0x04         NetBSD says 0x30
 }
 
 final class Ch340gConstants {
 
+    @Suppress("MagicNumber")
     public fun map(): Map<String, String> {
         val (divisorPrescale, mod) = baudRate(9600)
         return mapOf(
@@ -31,17 +34,19 @@ final class Ch340gConstants {
         )
     }
 
+    @Suppress("MagicNumber")
     private fun wordFromBytes(highByte: Int, lowByte: Int): Int {
         return lowByte * 256 + highByte
     }
 
     /* For details of sources, etc, see:
-     * src/shared/src/commonMain/kotlin/gpo_746/Ch340g.kt_template
+     * src/shared/src/commonMain/kotlin/gpo746/Ch340g.kt_template
      *
      * There's also a very interesting, but quite long, explanation of baud rate
      * calculation at:
      * https://github.com/nospam2000/ch341-baudrate-calculation
      */
+    @Suppress("MagicNumber", "ThrowsCount")
     public fun baudRate(rate: Int): List<Int> {
         val basis = mapOf(
             2400 to listOf(93750, 1),
@@ -52,18 +57,16 @@ final class Ch340gConstants {
             115200 to listOf(6000000, 3),
         )
 
-        if (!basis.containsKey(rate)) {
-            throw Exception("Invalid baud rate ${rate} not in ${basis.keys}")
+        check(basis.containsKey(rate)) {
+            "Invalid baud rate $rate not in ${basis.keys}"
         }
 
         val (clock, scalar) = basis[rate]!!
         val remainder = clock % rate
         var dividend = clock / rate
 
-        if (dividend == 0 || dividend >= 0xFF) {
-            // In theory, this exception will never be thrown????
-            throw Exception("Baud rate divider overflow!")
-        }
+        // In theory, this should never go wrong.
+        check(dividend > 0 && dividend <= 0xFF) { "Baud rate divider overflow" }
 
         if (remainder * 2 >= rate) {
             dividend = dividend + 1
@@ -77,9 +80,7 @@ final class Ch340gConstants {
         mod = mod + mod / 2
         mod = (mod + 0xFF) / 0x100
 
-        if (mod < 0 || mod >= 0xFF) {
-            throw Exception("Baud rate modulus overflow!");
-        }
+        check(mod >= 0 && mod <= 0xFF) { "Baud rate modulus overflow!" }
 
         return listOf(
             wordFromBytes(scalar, dividend),
@@ -96,8 +97,8 @@ final class Ch340gConstants {
 
     public fun defaultLcr(): Int {
         return lcr(
-            setOf(Lcr1Bit.enableRX, Lcr1Bit.CS8),
-            setOf(Lcr2Bit.parityNone)
+            setOf(Lcr1Bit.EnableRX, Lcr1Bit.CS8),
+            setOf(Lcr2Bit.ParityNone)
         )
     }
 }

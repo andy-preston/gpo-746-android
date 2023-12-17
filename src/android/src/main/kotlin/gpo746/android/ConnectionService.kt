9166433c112517bc80b5d75972a9441f8f6ec8e1
Andy Preston <edgeeffect@gmail.com>
2023-12-17 11:29:57 +0000

    ConnectionService compiles at least

--- src/android/src/main/kotlin/gpo746/android/ConnectionService.kt
+++ src/android/src/main/kotlin/gpo746/android/ConnectionService.kt
@@ -1,32 +1,59 @@
 package andyp.gpo746.android
 
-import android.telecom.Connection 
-import android.telecom.ConnectionRequest 
+import android.telecom.CallAudioState
+import android.telecom.Connection as BaseConnection
+import android.telecom.ConnectionRequest
 import android.telecom.ConnectionService as BaseConnectionService
-import android.telecom.PhoneAccountHandle 
+import android.telecom.PhoneAccountHandle
+
+class Connection: BaseConnection() {
+    override public fun onShowIncomingCallUi() {
+    }
+
+    // This method was deprecated in API level 34
+    override public fun onCallAudioStateChanged(state: CallAudioState) {
+    }
+
+    override public fun onHold() {
+    }
+
+    override public fun onUnhold() {
+    }
+
+    override public fun onAnswer() {
+    }
+
+    override public fun onReject() {
+    }
+
+    override public fun onDisconnect() {
+    }
+}
 
 class ConnectionService : BaseConnectionService() {
     override public fun onCreateOutgoingConnection(
         connectionManagerPhoneAccount: PhoneAccountHandle,
-        request: ConnectionRequest 
-    ): Connection {
+        request: ConnectionRequest
+    ): BaseConnection {
+        return Connection()
     }
 
     override public fun onCreateOutgoingConnectionFailed(
         connectionManagerPhoneAccount: PhoneAccountHandle,
-        request: ConnectionRequest 
+        request: ConnectionRequest
     ) {
     }
 
     override public fun onCreateIncomingConnection(
         connectionManagerPhoneAccount: PhoneAccountHandle,
-        request: ConnectionRequest 
-    ): Connection {
+        request: ConnectionRequest
+    ): BaseConnection {
+        return Connection()
     }
-    
+
     override public fun onCreateIncomingConnectionFailed(
         connectionManagerPhoneAccount: PhoneAccountHandle,
-        request: ConnectionRequest 
+        request: ConnectionRequest
     ) {
     }
 }

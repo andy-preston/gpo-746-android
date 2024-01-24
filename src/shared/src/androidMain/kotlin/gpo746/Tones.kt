8009a510a113a58a350f5c3ca99488ae2e6d622c
Andy Preston <edgeeffect@gmail.com>
2024-01-24 21:58:27 +0000

    Added support for engaged tone

--- src/shared/src/androidMain/kotlin/gpo746/Tones.kt
+++ src/shared/src/androidMain/kotlin/gpo746/Tones.kt
@@ -1,35 +1,20 @@
 package andyp.gpo746
 
-import android.media.AudioAttributes
 import android.media.AudioFormat
-import android.media.AudioManager
 import android.media.AudioTrack
 import android.util.Log
-import java.io.ByteArrayOutputStream
 import java.lang.Thread
 
-abstract class ToneBufferBuilder {
-    protected fun setupSamples(minimumSize: Int, waveform: ByteArray): ByteArray {
-        var bufferBytes = 0
-        val waveBytes = waveform.size
-        val stream = ByteArrayOutputStream()
-        while (bufferBytes < minimumSize) {
-            bufferBytes = bufferBytes + waveBytes
-            stream.write(waveform)
-        }
-        return stream.toByteArray()
-    }
-}
+enum class ToneSelection { dial, misdial, engaged }
 
 const val SAMPLE_RATE = 8000 // Hz
 
 final class Tones : ToneBufferBuilder() {
-    private var playing: AudioTrack? = null
     private var stopped: Boolean = true
-    private val dialSamples: ByteArray
-    private val misdialSamples: ByteArray
-    private val dialTrack: AudioTrack
-    private val misdialTrack: AudioTrack
+    private var playing: ToneSelection? = null
+    private val dialTone: Tone
+    private val misdialTone: Tone
+    private val engagedTone: Tone
 
     init {
         val minBufferSize = AudioTrack.getMinBufferSize(
@@ -37,35 +22,52 @@ final class Tones : ToneBufferBuilder() {
             AudioFormat.CHANNEL_OUT_MONO,
             AudioFormat.ENCODING_PCM_8BIT
         )
-        val toneSamples = ToneSamples()
-        dialSamples = setupSamples(minBufferSize, toneSamples.dial)
-        misdialSamples = setupSamples(minBufferSize, toneSamples.misdial)
-        dialTrack = audioTrack(dialSamples.size)
-        misdialTrack = audioTrack(misdialSamples.size)
+        dialTone = Tone(minBufferSize, byteArrayOf(
+            /* I really should do a Fourier analysis of this and work out
+             * what it's made of. I started with a 32bit float 44.1
+             * recording and cut it down, dropped the word-length and
+             * down-sampled it checking it still sounded right and it's
+             * still fine.
+             */
+            4, 43, 55, 67, 76, 67, 60, 38, 24, 2,
+            -17, -33, -36, -41, -31, -22, -10,
+            7, 16, 28, 36, 31, 29, 22, 1,
+            -8, -22, -30, -34, -33, -21, -20, -9,
+            39, 38, 51, 72, 67, 66, 70, 36,
+            /* This little `-9, 0,` "blip" offended my sense of symmetry and
+             * I tried to cut it out and it drastically altered the timbre.
+             * I really should do a Fourier analysis.
+             */
+            -9, 0,
+            -46, -65, -83, -93, -79, -83, -32, -13,
+            20, 59, 64, 116, 98, 111, 93, 80, 60,
+            -16, -6, -78, -69, -90, -118, -83, -84, -42, -26,
+            17, 47, 49, 91, 93, 88, 92, 66, 35, 20,
+            -10, -54, -48, -67, -61, -56, -46, -19, -8
+        ))
+        misdialTone = Tone(minBufferSize, byteArrayOf(
+            0, -39, -68, -108, -108, -108, -82, -52, -16, -78, -12,
+            44, 60, 96, 96, 96, 80, 53, 73, 77,
+            -26, -39, -88, -108, -111, -98, -78, -32, -32, -78,
+            21, 44, 73, 96, 100, 93, 73, 50, 90, 47,
+            -39, -49, -105, -105, -115, -92, -72, -16, -59, -52,
+            37, 47, 86, 96, 100, 86, 63, 53, 96, 11
+        ))
+        engagedTone = Tone(minBufferSize, byteArrayOf(
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ))
     }
-
-    private fun audioTrack(samplesSize: Int) = AudioTrack(
-        AudioAttributes.Builder().setUsage(
-            AudioAttributes.USAGE_MEDIA
-        ).setContentType(
-            AudioAttributes.CONTENT_TYPE_SONIFICATION
-        ).build(),
-        AudioFormat.Builder().setEncoding(
-            AudioFormat.ENCODING_PCM_8BIT
-        ).setChannelMask(
-            AudioFormat.CHANNEL_OUT_MONO
-        ).setSampleRate(
-            SAMPLE_RATE
-        ).build(),
-        samplesSize,
-        AudioTrack.MODE_STREAM,
-        AudioManager.AUDIO_SESSION_ID_GENERATE
-    )
 
     public fun finish() {
         stop()
-        misdialTrack.release()
-        dialTrack.release()
+        misdialTone.release()
+        dialTone.release()
+        engagedTone.release()
     }
 
     public fun isPlaying(): Boolean {
@@ -76,22 +78,25 @@ final class Tones : ToneBufferBuilder() {
         while (!stopped) { playing = null }
     }
 
-    public fun play(misdial: Boolean) {
-        val track = if (misdial) misdialTrack else dialTrack
-        if (playing == track) {
+    public fun play(selection: ToneSelection) {
+        if (selection == playing) {
             return
         }
-        val samples = if (misdial) misdialSamples else dialSamples
+        val tone = when (selection) {
+           ToneSelection.dial -> dialTone
+           ToneSelection.misdial -> misdialTone
+           ToneSelection.engaged -> engagedTone
+        }
         stop()
         Thread({
             var starting: Boolean = true
-            playing = track
+            playing = selection
             stopped = false
             try {
                 while (playing != null) {
-                    track.write(samples, 0, samples.size)
+                    tone.write()
                     if (starting) {
-                        track.play()
+                        tone.start()
                         starting = false
                     }
                 }
@@ -100,7 +105,7 @@ final class Tones : ToneBufferBuilder() {
             } catch (e: OutOfMemoryError) {
                 Log.e("gpo746", e.toString())
             }
-            track.stop()
+            tone.stop()
             stopped = true
         }).start()
     }

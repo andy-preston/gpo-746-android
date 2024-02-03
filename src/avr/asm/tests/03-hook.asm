5a2ab3f28ede44560c7713a83803da2cc81b87bc
Andy Preston <edgeeffect@gmail.com>
2024-02-03 19:13:04 +0000

    add more details to the test procedures

--- src/avr/asm/tests/03-hook.asm
+++ src/avr/asm/tests/03-hook.asm
@@ -8,8 +8,9 @@
     ; 1. Using just the microcontroller board (with it's power jumper shorted),
     ;    short pin `pin_in_receiver` to ground and the LED will light.
     ;
-    ; 2. Using the switching board attached (with it's power jumper missing),
-    ;    short the hook pins and the LED will light.
+    ; 2. Attach the switching board (with it's power jumper missing)
+    ;    connecting it's hook output to `pin_in_receiver`. Then short the
+    ;    switching board's hook input pins and the LED will light.
     ;
     ; 3. With the telephone hook attached, The light will be on by default and
     ;    picking up the phone will cause the LED to go off.
--- src/avr/asm/tests/04-dial-active.asm
+++ src/avr/asm/tests/04-dial-active.asm
@@ -9,11 +9,14 @@
     ;    short `pin_in_dial_grey` to VCC and the LED will light. Short
     ;    `pin_in_dial_grey` to ground and the LED will go out.
     ;
-    ; 2. Using the switching board attached (with it's power jumper missing),
-    ;    short the "grey" pin to VCC and the LED will light.
+    ; 2. Attach the switching board (with it's power jumper missing), by
+    ;    connecting it's dial active output to `pin_in_dial_grey`
+    ;    short the "grey" pin of the switching board's dial input to VCC and
+    ;    the LED will light.
     ;
-    ; 3. With "grey" and and "blue" on the dial attached, dial a digit and as
-    ; the dial is in motion the LED will light.
+    ; 3. With "grey" and and "blue" of the dial attached to the switching
+    ;    board's inputs, dial a digit and when the dial is in motion the LED
+    ;    will light.
 
     setup_outputs
     blink_off

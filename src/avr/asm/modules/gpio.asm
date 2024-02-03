ee1167dadf11cd78c6dfa01553afe3c343a24a63
Andy Preston <edgeeffect@gmail.com>
2024-02-03 12:51:17 +0000

    bit of formatting in GPIO defs

--- src/avr/asm/modules/gpio.asm
+++ src/avr/asm/modules/gpio.asm
@@ -7,14 +7,18 @@
     ; Switching the RI handshaking line on the CH340G to signal to the other
     ; side that the hook is up.
     .equ pin_out_RI = 0
+
     ; Two pins to switch the bells. This should be an AC sine wave. But the
     ; square wave you get out of GPIO seems to sound alright.
     .equ pin_out_ding = 1
     .equ pin_out_dong = 2
+
     ; "Internal" output to switch the amplifier on or off.
     .equ pin_out_amplifier = 3
+
     ; Diagnostic LED free for blinking for whatever may like to blink it.
     .equ pin_out_LED = 4
+
     ; These are the last output pins to use, just in case,
     ; in some point in the future, we might want an in-circuit programmer.
     .equ pin_out_MOSI_B5 = 5
@@ -29,19 +33,25 @@
     ; "in an emergency", we can use that as an input.
     .equ pin_in_RX_D0 = 0
     .equ pin_in_TX_D1 = 1
+
     ; "Internal" input for the receiver hook.
     .equ pin_in_receiver = 2
+
     ; The dial's grey line switches to blue (VCC) when the dial is active.
     .equ pin_in_dial_grey = 3
+
     ; D4 isn't used as a GPIO but for as the T0 pin for TimerCounter0 to count
     ; pulses from the dial. See ./dial-counter.asm` for usage details.
     ; The pink line is switched to orange (GND) for 50ms for each pulse.
     .equ pin_in_dial_pink = 4
+
     ; Used to read the RTS line on the CH340G to see if we should ring or not.
     .equ pin_in_RTS = 5
+
     ; Used to read the DTR line on the CH340G to see if the amplifier should be
     ; on or not.
     .equ pin_in_DTR = 6
+
     ; D7 is going spare at the moment.
     .equ pin_in_absent_D7 = 7
 

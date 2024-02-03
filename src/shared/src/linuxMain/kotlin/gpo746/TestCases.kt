7b323d9f46599982ab5963b92b110068d2c9f73c
Andy Preston <edgeeffect@gmail.com>
2024-02-03 21:33:57 +0000

    needless tweaking of linux test cases

--- src/shared/src/linuxMain/kotlin/gpo746/TestCases.kt
+++ src/shared/src/linuxMain/kotlin/gpo746/TestCases.kt
@@ -4,6 +4,10 @@ class TestCases(driver: Ch340g) {
     private val ch340g = driver
     private var flipBit = false
 
+    private fun highLow(bit: Boolean, line: String): String {
+        return if (flipBit) "$line 5V" else "$line Gnd"
+    }
+
     private fun serialInput() {
         val string = ch340g.readSerial()
         println(">>>$string<<<")
@@ -11,21 +15,18 @@ class TestCases(driver: Ch340g) {
 
     private fun rtsOutput() {
         flipBit = !flipBit
-        val state = if (flipBit) "5V" else "Gnd"
-        println("RTS $state")
+        println(highLow(flipBit, "RTS"))
         ch340g.writeHandshake(flipBit, false)
     }
 
     private fun dtrOutput() {
         flipBit = !flipBit
-        val state = if (flipBit) "5V" else "Gnd"
-        println("DTR $state")
+        println(highLow(flipBit, "DTR"))
         ch340g.writeHandshake(false, flipBit)
     }
 
     private fun riInput() {
-        val state = if (ch340g.readHandshake()) "5V" else "Gnd"
-        println("RI $state")
+        println(highLow(ch340g.readHandshake(), "RI"))
     }
 
     public fun list(): Map<String, () -> Unit> {

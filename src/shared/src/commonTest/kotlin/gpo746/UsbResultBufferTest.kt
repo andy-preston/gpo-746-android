61046266f1c65c8230419b5daa3972144f2c5bb6
Andy Preston <edgeeffect@example.com>
2024-03-14 19:51:41 +0000

    Formatting

--- src/shared/src/commonTest/kotlin/gpo746/UsbResultBufferTest.kt
+++ src/shared/src/commonTest/kotlin/gpo746/UsbResultBufferTest.kt
@@ -4,7 +4,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @OptIn(kotlin.ExperimentalUnsignedTypes::class)
-class UsbResultBufferTest() : UsbResultBuffer() {
+class UsbResultBufferTest : UsbResultBuffer() {
 
     private fun assertEqualsHex(expected: String, value: UShort) {
         assertEquals(expected, value.toString(16).uppercase())
@@ -16,15 +16,33 @@ class UsbResultBufferTest() : UsbResultBuffer() {
 
     @Test
     public fun shortFromBuffer_gives_a_UShort_of_the_first_2_bytes_in_a_buffer() {
-        assertEqualsHex("C0DE", shortFromBuffer(
-            byteArrayOf(aByte(0xDE), aByte(0xC0))
-        ))
-        assertEqualsHex("CAFE", shortFromBuffer(
-            byteArrayOf(aByte(0xFE), aByte(0xCA))
-        ))
-        assertEqualsHex("F00D", shortFromBuffer(
-            byteArrayOf(aByte(0x0D), aByte(0xF0))
-        ))
+        assertEqualsHex(
+            "C0DE",
+            shortFromBuffer(
+                byteArrayOf(
+                    aByte(0xDE),
+                    aByte(0xC0)
+                )
+            )
+        )
+        assertEqualsHex(
+            "CAFE",
+            shortFromBuffer(
+                byteArrayOf(
+                    aByte(0xFE),
+                    aByte(0xCA)
+                )
+            )
+        )
+        assertEqualsHex(
+            "F00D",
+            shortFromBuffer(
+                byteArrayOf(
+                    aByte(0x0D),
+                    aByte(0xF0)
+                )
+            )
+        )
     }
 
     @Test

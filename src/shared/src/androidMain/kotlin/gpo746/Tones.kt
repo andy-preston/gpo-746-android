a2880e099fe85397e9cde4f45bf2beb1fb107e19
Andy Preston <edgeeffect@gmail.com>
2024-03-19 21:28:59 +0000

    The tone player is still buggy but at least I have some idea why now

--- src/shared/src/androidMain/kotlin/gpo746/Tones.kt
+++ src/shared/src/androidMain/kotlin/gpo746/Tones.kt
@@ -1,12 +1,55 @@
 package andyp.gpo746
 
+import android.media.AudioAttributes
 import android.media.AudioFormat
+import android.media.AudioManager
 import android.media.AudioTrack
+import java.io.ByteArrayOutputStream
 import java.lang.Thread
 
 enum class ToneSelection { DIAL, MISDIAL, ENGAGED }
 
-final class Tones : ToneData() {
+internal final class Tone(
+    sampleArray: ByteArray,
+    audioAttributes: AudioAttributes,
+    audioFormat: AudioFormat
+) {
+
+    private val samples = sampleArray
+
+    private val track = AudioTrack(
+        audioAttributes,
+        audioFormat,
+        samples.size,
+        AudioTrack.MODE_STREAM,
+        AudioManager.AUDIO_SESSION_ID_GENERATE
+    )
+
+    public fun release() = track.release()
+
+    public fun write() = track.write(samples, 0, samples.size)
+
+    public fun start() = track.play()
+
+    public fun stop() = track.stop()
+}
+
+abstract class ToneBufferBuilder {
+
+    protected fun setupSamples(minimumSize: Int, waveform: ByteArray): ByteArray {
+        var bufferBytes = 0
+        val waveBytes = waveform.size
+        val stream = ByteArrayOutputStream()
+        while (bufferBytes < minimumSize) {
+            bufferBytes = bufferBytes + waveBytes
+            stream.write(waveform)
+        }
+        return stream.toByteArray()
+    }
+}
+
+final class Tones : ToneBufferBuilder(), ToneData {
+
     private var thread: Thread? = null
     private var playing: ToneSelection? = null
     private val dialTone: Tone
@@ -19,9 +62,38 @@ final class Tones : ToneData() {
             AudioFormat.CHANNEL_OUT_MONO,
             AudioFormat.ENCODING_PCM_8BIT
         )
-        dialTone = Tone(minBufferSize, dialToneData)
-        misdialTone = Tone(minBufferSize, misdialToneData)
-        engagedTone = Tone(minBufferSize, engagedToneData)
+
+        val audioAttributes = AudioAttributes.Builder().setUsage(
+            AudioAttributes.USAGE_MEDIA
+        ).setContentType(
+            AudioAttributes.CONTENT_TYPE_SONIFICATION
+        ).build()
+
+        val audioFormat = AudioFormat.Builder().setEncoding(
+            AudioFormat.ENCODING_PCM_8BIT
+        ).setChannelMask(
+            AudioFormat.CHANNEL_OUT_MONO
+        ).setSampleRate(
+            SAMPLE_FREQUENCY
+        ).build()
+
+        dialTone = Tone(
+            setupSamples(minBufferSize, dialToneData()),
+            audioAttributes,
+            audioFormat
+        )
+
+        misdialTone = Tone(
+            setupSamples(minBufferSize, misdialToneData()),
+            audioAttributes,
+            audioFormat
+        )
+
+        engagedTone = Tone(
+            setupSamples(minBufferSize, engagedToneData()),
+            audioAttributes,
+            audioFormat
+        )
     }
 
     public fun finish() {
@@ -31,9 +103,7 @@ final class Tones : ToneData() {
         engagedTone.release()
     }
 
-    public fun isPlaying(): Boolean {
-        return playing != null || thread != null
-    }
+    public fun isPlaying(): Boolean = (playing != null || thread != null)
 
     public fun stop() {
         playing = null

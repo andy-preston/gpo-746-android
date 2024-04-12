2648ccf871b6b72def58772fc98567bbc48f2315
Andy Preston <edgeeffect@gmail.com>
2024-04-12 23:07:58 +0100

    basic layout of icon scaler task

--- src/buildSrc/src/main/kotlin/gpo746/IconScaler.kt
+++ src/buildSrc/src/main/kotlin/gpo746/IconScaler.kt
@@ -1,31 +1,69 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.Directory
+import org.gradle.api.tasks.TaskAction
 import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-internal final class IconScaler(sourceName: String) {
+open class IconScaler : DefaultTask() {
 
-    private val sourceImage: BufferedImage = ImageIO.read(File(sourceName))
+    private lateinit var resourceDirectory: Directory
 
-    public fun resizeImage(
-        destinationName: String,
-        scaleWidth: Int,
-        scaleHeight: Int
-    ) {
-        val scaledImage = sourceImage.getScaledInstance(
-            scaleWidth,
-            scaleHeight,
+    private lateinit var sourceUrl: String
+
+    private var sourceImageCache: BufferedImage? = null
+
+    private val sizes = mapOf(
+        "mdpi" to 48,
+        "hdpi" to 72,
+        "xhdpi" to 96,
+        "xxhdpi" to 144,
+        "xxxhdpi" to 192
+    )
+
+    public fun directory(dir: Directory) {
+        resourceDirectory = dir
+    }
+
+    public fun url(u: String) {
+        sourceUrl = u
+    }
+
+    private fun destinationFile(directoryName: String): File {
+        return resourceDirectory.dir(directoryName).file("ic_launcher.png").asFile
+    }
+
+    private fun sourceImage(): BufferedImage {
+        if (sourceImageCache == null) {
+            sourceImageCache = ImageIO.read(File(sourceUrl))
+        }
+        return sourceImageCache!!
+    }
+
+    @TaskAction
+    public fun taskAction() {
+        val files = resourceDirectory.asFile.list()
+        for ((sizeName, size) in sizes) {
+            val directoryName = "mipmap-$sizeName"
+            if (!files.contains(directoryName)) {
+                resizeImage(destinationFile(directoryName), size)
+            }
+        }
+    }
+
+    public fun resizeImage(destinationFile: File, scaleSize: Int) {
+        val scaledImage = sourceImage().getScaledInstance(
+            scaleSize,
+            scaleSize,
             Image.SCALE_SMOOTH
         )
         val destinationImage = BufferedImage(
-            scaleWidth,
-            scaleHeight,
+            scaleSize,
+            scaleSize,
             BufferedImage.TYPE_INT_RGB
         )
         destinationImage.getGraphics().drawImage(scaledImage, 0, 0, null)
-        // make sure that outputFile exists.
-        // If it doesn't, write() will throw a NullPointerException
-        val destinationFile = File(destinationName)
         ImageIO.write(destinationImage, "jpg", destinationFile)
     }
 }

f055bd0cf4bbfd3bc4c11b03e6d738b2a952b85b
Andy Preston <edgeeffect@gmail.com>
2024-04-18 10:18:50 +0100

    Only check dial status when usb is connected

--- src/android/src/main/kotlin/gpo746/android/OutgoingActivity.kt
+++ src/android/src/main/kotlin/gpo746/android/OutgoingActivity.kt
@@ -33,7 +33,7 @@ abstract class OutgoingActivity : IncomingActivity() {
     public override fun onStart() {
         super.onStart()
         logInfo("OutgoingActivity", "onStart")
-        //pollOutgoing()
+        pollOutgoing()
     }
 
     private val toneClickListener = object : View.OnClickListener {
@@ -48,35 +48,39 @@ abstract class OutgoingActivity : IncomingActivity() {
 
     protected override fun pollOutgoing() {
         logInfo("OutgoingActivity", "pollOutgoing")
-        if (hookIsUp()) {
-            when (phoneNumber.digits(ch340g.readSerial())) {
-                ValidatorResult.Invalid -> invalidNumber()
-                ValidatorResult.Incomplete -> incompleteNumber()
-                ValidatorResult.Good -> dialNumber()
+        if (connectedIndicator.isChecked()) {
+            if (hookIsUp()) {
+                dialing()
+            } else {
+                noDialing()
             }
-        } else {
-            phoneNumber.clear()
-            tones.stop()
-            outputMode(ring = false, amp = false)
         }
         numberDisplay.apply { text = phoneNumber.number() }
         super.pollOutgoing()
     }
 
-    private fun invalidNumber() {
-        tones.play(ToneSelection.MISDIAL)
-        outputMode(ring = false, amp = true)
-    }
-
-    private fun incompleteNumber() {
-        tones.play(ToneSelection.DIAL)
-        outputMode(ring = false, amp = true)
-    }
-
-    private fun dialNumber() {
+    private fun noDialing() {
+        phoneNumber.clear()
         tones.stop()
-        val intent = Intent(Intent.ACTION_CALL)
-        intent.data = Uri.parse("tel:" + phoneNumber.number())
-        startActivity(intent)
+        outputMode(ring = false, amp = false)
+    }
+
+    private fun dialing() {
+        when (phoneNumber.digits(ch340g.readSerial())) {
+            ValidatorResult.Invalid -> {
+                tones.play(ToneSelection.MISDIAL)
+                outputMode(ring = false, amp = true)
+            }
+            ValidatorResult.Incomplete -> {
+                tones.play(ToneSelection.DIAL)
+                outputMode(ring = false, amp = true)
+            }
+            ValidatorResult.Good -> {
+                tones.stop()
+                val intent = Intent(Intent.ACTION_CALL)
+                intent.data = Uri.parse("tel:" + phoneNumber.number())
+                startActivity(intent)
+            }
+        }
     }
 }
